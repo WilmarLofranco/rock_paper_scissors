2831c6a04c03380cdfc5fe6ed0e28687799505a3
Wilmar <wilmarlofranco@gmail.com>
2024-08-06 20:02:37 +0800

    Add UI through DOM manipulation

--- script.js
+++ script.js
@@ -1,96 +1,112 @@
 // Create function "getComputerChoice" which will randomly return one of the following string values: “rock”, “paper” or “scissors”.
-
+let computerChoice;
 function getComputerChoice () {
-    let randomNum = Math.floor(Math.random() * 3 + 1);
-    let computerInput;
-    if (randomNum === 1) {
-        computerInput = "rock"
+    let randomNum = Math.floor(Math.random() * 3);
+    if (randomNum === 0) {
+        computerChoice = "rock"
+    } else if (randomNum === 1) {
+        computerChoice = "paper"
     } else if (randomNum === 2) {
-        computerInput = "paper"
-    } else if (randomNum === 3) {
-        computerInput = "scissors"
-    }
-    return computerInput;   
-}
+        computerChoice = "scissors"
+    };
+    return (computerChoice);
+};
 
 //  Create function "getHumanChoice" w/c will return one of the valid choices depending on what the user inputs.
 
-function getHumanChoice () {
-    let humanInput = prompt("Rock, paper, or scissors?").toLowerCase();
-    return humanInput;
-};
+const buttons = document.querySelectorAll("button");
+let humanChoice;
+
+function getHumanChoice (event) {
+    humanChoice = event.target.id;
+    getComputerChoice();
+    roundsPlayed++;
+    round.textContent = `ROUND: ${roundsPlayed}`;
+    playRound(humanChoice, computerChoice);
+    };
+
+buttons.forEach(function(button) {
+    button.addEventListener("click", getHumanChoice);
+});
 
 // Declare the players score variables
 
 let humanScore = 0;
 let computerScore = 0;
+let roundsPlayed = 1;
 
 // write a function "playRound" that takes the human and computer player choices as arguments, plays a single round, increments the round winner’s score and logs a winner announcement.
 
+const result = document.querySelector("#results");
+const userScore = document.querySelector("#userScore");
+const comScore= document.querySelector("#comScore");
+const round = document.querySelector("#round");
+
 function playRound (humanChoice, computerChoice) {
+    
     if (humanChoice === "rock" && computerChoice === "paper") {
-        console.log("You lose! Paper beats rock!")
+        result.textContent= "Round Result: You lose! Paper beats rock!"
         computerScore++
     } else if (humanChoice === "paper" && computerChoice === "scissors") {
-        console.log("You lose! Scissors beats paper!")
+        result.textContent="Round Result: You lose! Scissors beats paper!"
         computerScore++
     } else if (humanChoice === "scissors" && computerChoice === "rock") {
-        console.log("You lose! Rock beats scissors!")
+        result.textContent="Round Result: You lose! Rock beats scissors!"
         computerScore++
     } else if (computerChoice === "rock" && humanChoice === "paper") {
-        console.log("You win! Paper beats rock!")
+        result.textContent="Round Result: You win! Paper beats rock!"
         humanScore++
     } else if (computerChoice === "paper" && humanChoice === "scissors") {
-        console.log("You win! Scissors beats paper!")
+        result.textContent="Round Result: You win! Scissors beats paper!"
         humanScore++
     } else if (computerChoice === "scissors" && humanChoice === "rock") {
-        console.log("You win! Rock beats scissors!")
+        result.textContent="Round Result: You win! Rock beats scissors!"
         humanScore++
     } else if (computerChoice === humanChoice) {
-        console.log("Draw! You have the same choice with the computer!")
+        result.textContent="Round Result: Draw! You have the same choice with the computer!"
     } else {
-        console.log("You have entered an invalid input!");
-        computerScore++};
-    console.log("Human Score:", humanScore);
-    console.log("Computer Score:", computerScore);
+        result.textContent="Round Result: You have entered an invalid input!";
+        computerScore++
+    };
+    userScore.textContent=`Human Score: ${humanScore}`;
+    comScore.textContent=`Computer Score: ${computerScore}`;
+    if (roundsPlayed > 5) {
+    endGame();
+};
 }
-
-let computerChoice = getComputerChoice();
-let humanChoice = getHumanChoice();
 
 // write a function named playGame that calls playRound to play 5 rounds, keeps track of the scores and declares a winner at the end
+const finalResult = document.querySelector("#finalResult")
 
-function playGame () {
-    console.log("Round 1!");
-    playRound (humanChoice, computerChoice);
-    
-    let humChoice2 = getHumanChoice();
-    let comChoice2 = getComputerChoice();
-    console.log("Round 2!");
-    playRound (humChoice2, comChoice2);
-    
-    let humChoice3 = getHumanChoice();
-    let comChoice3 = getComputerChoice();
-    console.log("Round 3!");
-    playRound (humChoice3, comChoice3);
-
-    let humChoice4 = getHumanChoice();
-    let comChoice4 = getComputerChoice();
-    console.log("Round 4!");
-    playRound (humChoice4, comChoice4);
-    
-    let humChoice5 = getHumanChoice();
-    let comChoice5 = getComputerChoice();
-    console.log("Round 5!");
-    playRound (humChoice5, comChoice5);
-
+function endGame() {
+    round.textContent = "GAME OVER!";
     if (humanScore > computerScore) {
-        console.log("Congratulations! You won!")
+        finalResult.textContent = "Final Result: Congratulations! You won!".toUpperCase();
     } else if (humanScore < computerScore) {
-        console.log("You lose! Refresh page to play again.")
-    } else if (humanScore === computerScore) {
-        console.log("Match draw! Refresh page to play again.")
-    }
-}
+        finalResult.textContent = "Final Result: You lose! Try Again!".toUpperCase();
+    } else {
+        finalResult.textContent = "Final Result: Match draw! Try Again!".toUpperCase();
+    };
+    userScore.textContent = `Final Human Score: ${humanScore}`;
+    comScore.textContent = `Final Computer Score: ${computerScore}`;
+    buttons.forEach(function(button) {
+    button.removeEventListener("click", getHumanChoice);
+    })
+};
 
-playGame ();
+const reset = document.querySelector("#reset");
+reset.addEventListener("click", resetGame)
+
+function resetGame () {
+    humanScore = 0;
+    computerScore = 0;
+    roundsPlayed = 1;
+    userScore.textContent = `Human Score: ${humanScore}`;
+    comScore.textContent = `Computer Score: ${computerScore}`;
+    result.textContent = "Waiting for you to pick...";
+    finalResult.textContent = "";
+    round.textContent = `ROUND: ${roundsPlayed}`;
+    buttons.forEach(function(button) {
+    button.addEventListener("click", getHumanChoice);
+    })
+};
